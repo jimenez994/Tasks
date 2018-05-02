fae85a9c34e100039837c1f620f108ae68c6a2f2
Jonathan Jimenez <j.jimenez.994@hotmail.com>
2018-05-02 19:51:52 -0400

    _

--- Tasks/src/main/java/com/example/Tasks/controllers/TaskController.java
+++ Tasks/src/main/java/com/example/Tasks/controllers/TaskController.java
@@ -3,6 +3,7 @@ package com.example.Tasks.controllers;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,7 @@ import com.example.Tasks.models.Task;
 import com.example.Tasks.services.TaskService;
 
 @RestController
-//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/")
 public class TaskController {
 	
